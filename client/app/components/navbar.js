6cfdf631ae8ce8cf9d6f5ae8e7f0583cdf0f3269
Sophia Cai <cai_sophia@outlook.com>
2024-09-29 18:32:07 -0400

    added dynamic rendering to navbar based on active pages

--- client/app/components/navbar.js
+++ client/app/components/navbar.js
@@ -1,27 +1,45 @@
+"use client";
 import React from "react";
 import Link from "next/link"; 
 import styles from "./navbar.module.css"; 
+import { usePathname } from "next/navigation"; 
 
 const Navbar = () => {
+    const pathname = usePathname();
+    const isLoginPage = pathname === "/login"
+    const isRegisterPage = pathname === "/register"
+
+    const isActiveLink = (linkPath) => pathname === linkPath;
+
     return (
       <div className={styles.navWrapper}>
-        <div className={styles.navTitle}>
+        <Link className={styles.navTitle} href="/">
             <img className={styles.navLogo} src="/assets/logo.png"/>
-            <div>PANTRY<span class={styles.palText}>PAL</span></div>
-        </div>
-        <div className={styles.navLinksWrapper}>
-            <Link href="/">Home</Link>
-            <Link href="/pantry">Pantry</Link>
-            <Link href="/discover">Discover</Link>
-        </div>
-        <div className={styles.navLinksWrapper}>
+            <div>PANTRY<span className={styles.palText}>PAL</span></div>
+        </Link>
+        {!isLoginPage && !isRegisterPage && (
+        <>
+          <div className={styles.navLinksWrapper}>
+            <Link href="/" className={isActiveLink("/") ? styles.activeLink : ""}>
+              Home
+            </Link>
+            <Link href="/pantry" className={isActiveLink("/pantry") ? styles.activeLink : ""}>
+              Pantry
+            </Link>
+            <Link href="/discover" className={isActiveLink("/discover") ? styles.activeLink : ""}>
+              Discover
+            </Link>
+          </div>
+          <div className={styles.navLinksWrapper}>
             <Link href="/login">Log in</Link>
             <Link className={styles.navGetStarted} href="/register">
-                <div className={styles.navGetStartedText}>Get Started</div>
+              <div className={styles.navGetStartedText}>Get Started</div>
             </Link>
-        </div>
-      </div>
-    );
-  };
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
   
   export default Navbar;
