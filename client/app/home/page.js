50d6260794af4803edf2f6584d1aa2ce188a0b3f
AndyLi98 <116600525+AndyLi98@users.noreply.github.com>
2024-09-30 21:52:50 -0400

    clean up home page

--- client/app/home/page.js
+++ client/app/home/page.js
@@ -1,33 +1,3 @@
-// "use client";
-
-// import React from 'react';
-
-// const Home = () => {
-//   const handleSendEmail = async () => {
-//     try {
-//       const response = await fetch('http://localhost:8080/send-email');
-//       if (!response.ok) {
-//         throw new Error('Failed to send email');
-//       }
-//       const data = await response.json();
-//       alert(data.message);
-//     } catch (error) {
-//       console.error('Error:', error);
-//       alert('Error sending email: ' + error.message);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1>User personalized home page</h1>
-//       <button onClick={handleSendEmail}>Send Email</button>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
 "use client";
 
 import React, { useState } from 'react';
