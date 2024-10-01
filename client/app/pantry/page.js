75d749b6a0e3153a8dfd7a05c611a9551661d8a7
Riyuan Liu <riyuanliu31@gmail.com>
2024-10-01 18:33:11 -0400

    feat: add frontend and backend integration for ingredient addition with autocomplete
    What's in this change?:
    
    - Implemented frontend form using React for ingredient input and amount.
    - Connected the frontend to the backend API for adding ingredients to the database.
    - Added autocomplete functionality using Spoonacular API to suggest ingredients as users type.
    
    What's left to do?:
    
    -Additional testing to ensure seamless integration between frontend and backend.
    -Update documentation with API usage and autocomplete feature.
    
    Testing changes:
    
    - Frontend and backend tested locally with valid inputs.
    - Verified that new ingredients are correctly added to the database.
    - Autocomplete functionality tested with various ingredient inputs.
    
    Supplemental material (optional, screenshots of frontend changes, API testing, notifications):
    
    - N/A for now.
    
    Your commit message

--- client/app/pantry/page.js
+++ client/app/pantry/page.js
@@ -1,10 +1,13 @@
 import Navbar from "../components/navbar";
+import IngredientInput from './IngredientInput';
+import React from "react";
 
 const Pantry = () => {
     return (
       <div>
         <Navbar/>
         <h1>Pantry</h1>
+        <IngredientInput />
       </div>
     );
   };
