e477a6f994402b5478f9482e19aefd4ce6e5d715
Sophia Cai <cai_sophia@outlook.com>
2024-10-03 22:29:09 -0400

    Added basic components to login page + styling

--- client/app/login/page.js
+++ client/app/login/page.js
@@ -1,10 +1,22 @@
 import Navbar from "../components/navbar";
+import styles from './login.module.css'; 
+import TextField from '@mui/material/TextField';
+import Link from "next/link"; 
 
 const Login = () => {
     return (
       <div>
         <Navbar/>
-        <h1>User Login</h1>
+        <div className={styles.loginPageWrapper}>
+          <div className={styles.loginWrapper}>
+            <img src="../assets/logoTitle.png" className={styles.loginImage}/>
+            <div className={styles.loginTitle}>Login to your Pantry<span className={styles.titleOrange}>Pal</span> account</div>
+            <TextField required label="Email" variant="outlined" className={styles.loginEmail}/>
+            <TextField required label="Password" variant="outlined" type="password" className={styles.loginPassword}/>
+            <div className={styles.loginButton}>Log in</div>
+            <div className={styles.loginNoAcc}>Not registered? <Link className={styles.loginRegister} href="/register">Create an account</Link></div>
+          </div>
+        </div>
       </div>
     );
   };
