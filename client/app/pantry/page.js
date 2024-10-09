411c798172cdf6511c909ea608524d001e735b76
Sophia Cai <cai_sophia@outlook.com>
2024-10-09 01:59:11 -0400

    Creating css file, adding css

--- client/app/pantry/page.js
+++ client/app/pantry/page.js
@@ -1,15 +1,34 @@
 import Navbar from "../components/navbar";
 import IngredientInput from './IngredientInput';
 import React from "react";
+import styles from './pantry.module.css';
 
 const Pantry = () => {
-    return (
-      <div>
-        <Navbar/>
-        <h1>Pantry</h1>
-        <IngredientInput />
+  return (
+    <div>
+      <Navbar />
+      <div className={styles.pageWrapper}>
+        <div className={styles.title}>Welcome to your pantry</div>
+        <div className={styles.pantryContent}>
+          <div className={styles.sidebar}>
+            <div>
+              Ingredients
+            </div>
+            <div>
+              Allergens
+            </div>
+            <div>
+              Nutrition
+            </div>
+          </div>
+          <div className={styles.seperator}></div>
+          <div className={styles.ingredientInput}>
+            <IngredientInput />
+          </div>
+        </div>
       </div>
-    );
-  };
-  
-  export default Pantry;
+    </div>
+  );
+};
+
+export default Pantry;
