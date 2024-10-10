2e72a0516117d633274ffe71da87b3f1faff2cd5
JustinLau47 <justinlau47@gmail.com>
2024-10-10 16:04:02 -0400

    number of recipes displayed

--- client/app/recipe/recipe.js
+++ client/app/recipe/recipe.js
@@ -21,7 +21,7 @@ const RecipePage = () => {
                 params: {
                     apiKey: process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY,
                     ingredients: ingredients,
-                    number: 4
+                    number: 2
                 }
             });
 
