de3e06284d743eb8291e0d23e6241b09436321c4
Meheraan Khan <mhk7451@rit.edu>
2024-10-16 19:56:53 -0400

    removed more jest

--- server/server.js
+++ server/server.js
@@ -135,9 +135,7 @@ app.get('/get-low-ingredients', async (req, res) => {
 
 module.exports = app;
 
-if (process.env.NODE_ENV !== 'test') {
-    const port = process.env.PORT || 8080;
-    app.listen(port, () => {
-        console.log(`Server started on port ${port}`);
-    });
-};
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
