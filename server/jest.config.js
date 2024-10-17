bab5614eb9e84b278be9d9f843b9dd29df9ff678
AndyLi98 <116600525+AndyLi98@users.noreply.github.com>
2024-10-17 16:13:23 -0400

    yaml test 3

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,10 +1,10 @@
 require('dotenv').config({ path: './.env.test' });
 
 module.exports = {
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text', 'html'],
-  testTimeout: 10000,
-  testEnvironment: 'node',
-  verbose: true,
-};
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['lcov', 'text', 'html'],
+    testTimeout: 10000,
+    testEnvironment: 'node',
+    verbose: true,
+  };
