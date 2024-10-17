b9b7dc5b6f520c805bec135c381e9af01f889ee6
AndyLi98 <116600525+AndyLi98@users.noreply.github.com>
2024-10-16 23:45:17 -0400

    fix jest.congfig

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,14 +2,10 @@ require('dotenv').config({ path: './.env.test' });
 
 module.exports = {
   collectCoverage: true,
-
   coverageDirectory: 'coverage',
-
   coverageReporters: ['lcov', 'text', 'html'],
-
   testTimeout: 10000,
-
   testEnvironment: 'node',
-
   verbose: true,
+  testMatch: ['**/tests/**/*.test.js'],
 };
