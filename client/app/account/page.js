0b39a90ed0b02ea911440b7d817dc0e04a20bdfb
Sophia Cai <cai_sophia@outlook.com>
2024-10-30 16:29:21 -0400

    Adding profile page to account

--- client/app/account/page.js
+++ client/app/account/page.js
@@ -5,6 +5,7 @@ import IngredientInput from './IngredientInput'
 import NutritionInput from "./NutritionInput";
 import AllergenInput from "./AllergenInput";
 import RecipeHistory from './RecipeHistory';
+import Profile from './Profile'
 
 import React from "react";
 import styles from './account.module.css';
@@ -96,6 +97,12 @@ const Pantry = () => {
           >
             Recipe History
           </div>
+          <div
+            onClick={() => handleSectionClick('Profile')}
+            className={activeSection === 'Profile' ? styles.active : styles.notActive}
+          >
+            Profile
+          </div>
         </div>
         <div className={styles.pantrySection}>
           {activeSection === 'Pantry' && (
@@ -106,6 +113,7 @@ const Pantry = () => {
           {activeSection === 'Allergens' && <AllergenInput />}
           {activeSection === 'Nutrition' && <NutritionInput />}
           {activeSection === 'History' && <RecipeHistory />}
+          {activeSection === 'Profile' && <Profile />}
         </div>
       </div>
     </div>
