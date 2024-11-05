4626df454d847fbba8acbf4b6cf1cf30213a0c13
EbubeDivinee <oluebubeonwuamaegbu@gmail.com>
2024-11-05 10:36:02 -0500

    Reverted page.js in account folder

--- client/app/account/page.js
+++ client/app/account/page.js
@@ -1,5 +1,5 @@
 'use client';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 import Navbar from "../components/navbar";
 import IngredientInput from './IngredientInput';
@@ -13,7 +13,6 @@ import styles from './account.module.css';
 
 const Pantry = () => {
   const router = useRouter();
-  const searchParams = useSearchParams();
   const [loading, setLoading] = useState(true);
   const [authenticated, setAuthenticated] = useState(false);
   const [activeSection, setActiveSection] = useState('Pantry');
@@ -24,11 +23,6 @@ const Pantry = () => {
   };
 
   useEffect(() => {
-    const section = searchParams.get('section');
-    if (section) {
-      setActiveSection(section.charAt(0).toUpperCase() + section.slice(1));
-    }
-
     const token = localStorage.getItem('token');
 
     const verifyToken = async () => {
@@ -62,7 +56,7 @@ const Pantry = () => {
     };
 
     verifyToken();
-  }, [router, searchParams]);
+  }, [router]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -109,7 +103,11 @@ const Pantry = () => {
           </div>
         </div>
         <div className={styles.pantrySection}>
-          {activeSection === 'Pantry' && <IngredientInput />}
+          {activeSection === 'Pantry' && (
+            <>
+              <IngredientInput />
+            </>
+          )}
           {activeSection === 'Allergens' && <AllergenInput />}
           {activeSection === 'Nutrition' && <NutritionInput />}
           {activeSection === 'History' && <RecipeHistory />}
