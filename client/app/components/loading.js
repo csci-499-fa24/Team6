fe8e4bc48523bb08cc570c93b4b4a4387c47dc52
Sophia Cai <cai_sophia@outlook.com>
2024-11-12 17:25:34 -0500

    fixing casing

--- client/app/components/loading.js
+++ client/app/components/loading.js
@@ -3,7 +3,6 @@ import React, { } from "react";
 import styles from './loading.module.css';
 
 const LoadingScreen = ({ title = "" }) => {
-    console.log('loading')
     return (
         <div className={styles.loadingWrapper}>
             <img src="/assets/loading.gif" alt="Loading" className={styles.loadingAnimation} />
