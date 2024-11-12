08711f7777ac1f29dc9ecc21e409b543f39ffe30
Sophia Cai <cai_sophia@outlook.com>
2024-11-12 17:24:32 -0500

    fixing casing in file name

--- client/app/components/loading.js
+++ client/app/components/loading.js
@@ -3,6 +3,7 @@ import React, { } from "react";
 import styles from './loading.module.css';
 
 const LoadingScreen = ({ title = "" }) => {
+    console.log('loading')
     return (
         <div className={styles.loadingWrapper}>
             <img src="/assets/loading.gif" alt="Loading" className={styles.loadingAnimation} />
